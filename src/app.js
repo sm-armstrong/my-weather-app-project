c4bc892b733002e3086b1ff1a39a6fe56cba92fe
sm-armstrong <sapphiramk@gmail.com>
2024-06-26 15:14:40 +0100

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -2,9 +2,41 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature")
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let dateTimeElement = document.querySelector("#date-time");
+    let date = new Date(response.data.time * 1000);
 
+    dateTimeElement.innerHTML = formatDate(date);
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let dayDate = date.getDate();
+    let year = date.getFullYear();
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[date.getDay()];
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+      }
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let month = months[date.getMonth()];
+
+    return `${day} ${dayDate} ${month} ${year} ${hours}:${minutes}`
 }
 
 function searchCity(city) {
