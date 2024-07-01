5d6e5ba27aa7c24fb1c161aea131dd5448db6338
sm-armstrong <sapphiramk@gmail.com>
2024-07-01 14:55:18 +0100

    Injected the forecast HTML from JavaScript

--- src/app.js
+++ src/app.js
@@ -54,7 +54,32 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml += 
+            `
+                <div class="weather-forecast-info">
+                    <div class="weather-forecast-day">${day} 27</div>
+                    <div class="weather-forecast-icon">🌦️</div>
+                    <div class="weather-forecast-temperatures">
+                        <div class="weather-forecast-temperature">
+                            <strong>15°</strong> 
+                        </div>
+                        <div class="weather-forecast-temperature">9°</div>
+                    </div>
+                </div>
+            `;
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Brisbane");
+displayForecast();
